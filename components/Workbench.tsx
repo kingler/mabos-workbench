44dd5f826d9952088350ded8c9e97add13abac8c
Kingler Bercy <kingler@me.com>
2024-07-02 17:48:25 -0400

    Adjusted the text color in the Workbench component to use a lighter shade of gray.

--- components/Workbench.tsx
+++ components/Workbench.tsx
@@ -3,8 +3,8 @@ import { Box, Heading, Text } from '@radix-ui/themes';
 
 const Workbench = () => {
   return (
-    <Box className="workbench p-4 bg-gray-900 text-white">
-      <Heading size="4" className="mb-4">Workbench</Heading>
+    <Box className="workbench p-4 bg-gray-900 text-gray-100">
+      <Heading size="4" className="mb-4 text-gray-100">Workbench</Heading>
       <Text>Your workbench content goes here</Text>
     </Box>
   );
